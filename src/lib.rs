1f6ba13834821961abf3e59b679322048a29cf5e
schatzmeister <graf.schatzmeister@gmail.com>
2020-05-16 16:24:39 +0200

    Feat: Add `play` method to Game

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,24 @@
 use rand::seq::SliceRandom;
 use std::io;
 
+/// Game related error.
+#[derive(std::fmt::Debug, PartialEq, Eq)]
+struct GameError(String);
+
+impl std::fmt::Display for GameError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl std::error::Error for GameError {}
+
 /// Struct holding the complete data for one game.
 #[derive(Debug, Default, PartialEq, Eq)]
 struct Game {
     deck: Vec<u8>,
     hand: Vec<u8>,
-    // The stacks of cards
+    // The piles of cards to play on
     up1: Vec<u8>,
     up2: Vec<u8>,
     down1: Vec<u8>,
@@ -15,7 +27,8 @@ struct Game {
 
 impl std::fmt::Display for Game {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
-        write!(f,
+        write!(
+            f,
             "({decklen})|{up1} {up2} {down1} {down2}|{hand:?}",
             decklen = self.deck.len(),
             up1 = match self.up1.last() {
@@ -54,6 +67,27 @@ impl Game {
     fn draw(&mut self, amount: u8) {
         self.hand
             .extend(self.deck.split_off(self.deck.len() - (amount as usize)));
+    }
+
+    /// Play a card from the hand to a specific pile.
+    fn play(&mut self, card: &u8, pile: &str) -> Result<(), GameError> {
+        // Check if the pile is correct.
+        let pile = match pile {
+            "up1" => &mut self.up1,
+            "up2" => &mut self.up2,
+            "down1" => &mut self.down1,
+            "down2" => &mut self.down2,
+            s => return Err(GameError(format!("Pile `{}` does not exist.", s)))
+        };
+        // Check if the card exist in the hand.
+        match self.hand.iter().position(|x| x == card) {
+            Some(index) => {
+                pile.push(self.hand.remove(index));
+                Ok(()) 
+            },
+            None => Err(GameError(format!("Card `{}` not available.", card))),
+        }
+        // TODO: Check if the card is actually playable.
     }
 }
 
@@ -95,5 +129,14 @@ mod tests {
         let mut game = Game::new();
         game.draw(8);
         assert_eq!(game.hand.len(), 8);
+    }
+
+    #[test]
+    fn play_test() {
+        let mut game = Game::new();
+        game.draw(8);
+        game.play(game.hand.last().unwrap(), "up1");
+        assert_eq!(game.up1.len(), 1);
+        assert_eq!(game.hand.len(), 7);
     }
 }
